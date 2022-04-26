5a6067a4755123c49528163fc6059673a4746f0c
LaelBelchior <laelbelchior@gmail.com>
2022-04-26 19:50:44 -0300

    Instalando css-selector e criando buscar-cursos.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '742d1f894936e0287ada570d6eb4daef08e88f16',
+        'reference' => '0d908fb895d58a7d889011400ffe47a5f5a68268',
         'name' => 'laelbelchior/buscador-cursos',
         'dev' => true,
     ),
@@ -43,7 +43,7 @@
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '742d1f894936e0287ada570d6eb4daef08e88f16',
+            'reference' => '0d908fb895d58a7d889011400ffe47a5f5a68268',
             'dev_requirement' => false,
         ),
         'psr/http-client' => array(
@@ -98,6 +98,15 @@
             'install_path' => __DIR__ . '/../ralouphie/getallheaders',
             'aliases' => array(),
             'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+            'dev_requirement' => false,
+        ),
+        'symfony/css-selector' => array(
+            'pretty_version' => 'v5.4.3',
+            'version' => '5.4.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/css-selector',
+            'aliases' => array(),
+            'reference' => 'b0a190285cd95cb019237851205b8140ef6e368e',
             'dev_requirement' => false,
         ),
         'symfony/deprecation-contracts' => array(
