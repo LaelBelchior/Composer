8b9c286383d54afd88df1da04a977bf7174aca10
LaelBelchior <laelbelchior@gmail.com>
2022-04-27 15:15:04 -0300

    Configurando psr-4 no autoload no composer.json

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -7,7 +7,7 @@ use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['base_uri' => "https://www.alura.com.br/"]);
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
 $crawler = new Crawler();
 
 $buscador = new Buscador($client, $crawler);
@@ -15,5 +15,5 @@ $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach($cursos as $curso){
-    echo $curso -> textContent . PHP_EOL;
+    echo $curso . PHP_EOL;
 }
